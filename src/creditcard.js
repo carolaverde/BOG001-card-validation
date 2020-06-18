f49528866b1c8c454c8982cf22ee8abd6e66ac89
carolaverde <carolinalaverde5@gmail.com>
2020-06-18 12:46:24 -0500

    nuevos cambios css

--- src/creditcard.js
+++ src/creditcard.js
@@ -1,22 +1,19 @@
 import validator from './validator.js';
-document.getElementById("validar").addEventListener("click",final);
-function final(){
-    let numero = document.getElementById("tarjetaCredito").value;
-    document.getElementById("tarjetaCredito").value= validator.maskify(numero);
-    //console.log("Numero de tarjeta es: " + numero);
-    let resultado = validator.isValid(numero);
-    //console.log("Resultado es: " + resultado);
-    if (resultado == true){  
-    alert ("Gracias por su compra");
-        return true;
-    } else { 
-        alert("Tarjeta de crédito no valida");
-        return false;
-    }
+document.getElementById("validar").addEventListener("click", final);
+
+function final() {
+  let numero = document.getElementById("tarjetaCredito").value;
+  document.getElementById("tarjetaCredito").value = validator.maskify(numero);
+  //console.log("Numero de tarjeta es: " + numero);
+  let resultado = validator.isValid(numero);
+  //console.log("Resultado es: " + resultado);
+  if (resultado == true) {
+    alert("Gracias por su compra");
+    return true;
+  } else {
+    alert("Tarjeta de crédito no valida");
+    return false;
+  }
 
 
 }
-
-
-
-
